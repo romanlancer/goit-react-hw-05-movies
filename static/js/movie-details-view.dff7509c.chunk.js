67a161d1b22817cc3ac1c5092cf91df679794fd5
Roman Hyrych <roman.girich@gmail.com>
2022-05-17 02:53:09 +0300

    Updates

--- static/js/movie-details-view.dff7509c.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[353],{781:function(e,t,n){n.r(t),n.d(t,{default:function(){return r}});n(791);var i=n(184),r=function(){return(0,i.jsx)("div",{children:"sdfdsf"})}}}]);
-//# sourceMappingURL=movie-details-view.dff7509c.chunk.js.map
